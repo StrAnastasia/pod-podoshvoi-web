82dff6598ca97a37503621c1c8d0a1095f722950
eg-kruglikov <eg.kruglikov@yandex.ru>
2021-05-14 15:45:52 +0300

    pushka !

--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -1,3 +1,4 @@
+import zIndex from '@material-ui/core/styles/zIndex';
 import React from 'react';
 
 import { Link } from "react-router-dom";
@@ -8,10 +9,10 @@ import { Link } from "react-router-dom";
 export default function Header() {
 
     return (
-        <div className=''>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div style={{position: 'sticky', top: '0', zIndex: '1'}}>
+            <nav className="navbar navbar-fixed-top navbar-expand-lg navbar-dark bg-dark">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Under theSole</a>
+                   
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
