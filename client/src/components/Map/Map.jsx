1055e4f6ba55fcda52513e5efe5581323fd87ad7
tronn333 <tronn333@gmail.com>
2021-05-14 15:34:22 +0300

    map half-done

--- client/src/components/Map/Map.jsx
+++ client/src/components/Map/Map.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
+import { Loader } from "@googlemaps/js-api-loader"
 
 export default function Map() {
+    const loader = new Loader({
+        apiKey: "YOUR_API_KEY",
+        version: "weekly",
+        ...additionalOptions,
+      });
+      loader.load().then(() => {
+        map = new google.maps.Map(document.getElementById("map"), {
+          center: { lat: -34.397, lng: 150.644 },
+          zoom: 8,
+        });
+      });
     return (
         // <iframe src="https://www.google.com/maps/d/embed?mid=1NU5MOEVWU6iFzSSw5NJ3VDRIT75G1bay" width="640" height="480"></iframe>
         <iframe
