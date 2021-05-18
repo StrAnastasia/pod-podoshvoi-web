da136156ca8f3bdc9b2f42b5b84d49c629a0dfcd
tronn333 <tronn333@gmail.com>
2021-05-18 09:30:08 +0300

    map complete

--- client/src/components/Map/Map.jsx
+++ client/src/components/Map/Map.jsx
@@ -1,19 +1,7 @@
 import React, { useCallback, useRef, useState } from 'react'
 import { GoogleMap, InfoWindow, LoadScript,Marker } from '@react-google-maps/api'
 import mapStyle from './map'
-import {
-  Combobox,
-  ComboboxInput,
-  ComboboxPopover,
-  ComboboxList,
-  ComboboxOption,
-} from "@reach/combobox";
-import usePlacesAutocomplete, {
-  getGeocode,
-  getLatLng,
-} from "use-places-autocomplete";
 import "@reach/combobox/styles.css";
-import { formatRelative } from "date-fns";
 export default function Map() {
   const onMapClick = useCallback( (event)=>{
     setMarkers((current)=>[...current,
@@ -34,24 +22,28 @@ export default function Map() {
     mapRef.current.panTo({ lat, lng });
     mapRef.current.setZoom(14);
   }, []);
-
+  const selectHandler = (e) =>{
+    const newStr =JSON.parse(e.target.value)
+    console.log(newStr);
+    panTo(newStr.location)
+    setSelected(newStr)
+  }
   const [markers,setMarkers] = useState([])
   const [selected,setSelected] = useState(null)
     const containerStyle = {
-        width: '100vw',
-        height: '85vh'
+        width: '40vw',
+        height: '50vh'
       };
       const center = {
         lat: 55.70877845296291,
         lng: 37.58536453295382
       };
     return (
-        <>
+        <div style={{display:'flex'}}>
         <LoadScript
         googleMapsApiKey={'AIzaSyCtPbYjq1VPSnTlsfvfNs3pexwlEAYjDmk'}
         libraries = {["places"]}
         >
-           <Search panTo={panTo} />
        <GoogleMap
           mapContainerStyle={containerStyle}
           center={center}
@@ -69,7 +61,7 @@ export default function Map() {
         >
             {markers.map((el)=>{
               return(
-              <Marker
+                <Marker
               key={el.time}
               position={el.location}
               icon={{
@@ -77,75 +69,30 @@ export default function Map() {
                 scaledSize: new window.google.maps.Size(30,30),
               }}
               onClick={() =>{
+                console.log(el);
                 setSelected(el)
               }}
-            />)
+              />)
             })}
-            {selected? <InfoWindow position={{ lat: selected.location.lat, lng: selected.location.lng }} 
+            {selected? 
+            <InfoWindow position={{ lat: selected.location.lat, lng: selected.location.lng }} 
             onCloseClick={() =>{
-                setSelected(null)
-              }}>
+              setSelected(null)
+            }}>
               <div>
               <h2>Auf</h2>
               <p>reeeeeeee</p>
               </div>
             </InfoWindow>:null}
         </GoogleMap>
+                <select size='5' onChange={selectHandler}>
+                {markers.map((el)=>{
+                  const newStr = JSON.stringify(el)
+                 return (<option key={el.time} value={newStr} >{el.time}</option>)
+                })
+                }
+                </select>
       </LoadScript>
-       </>
+       </div>
     )
-}
-function Search({ panTo }) {
-  const {
-    ready,
-    value,
-    suggestions: { status, data },
-    setValue,
-    clearSuggestions,
-  } = usePlacesAutocomplete({
-    requestOptions: {
-      location: { lat: () => 43.6532, lng: () => -79.3832 },
-      radius: 100 * 1000,
-    },
-  });
-
-  // https://developers.google.com/maps/documentation/javascript/reference/places-autocomplete-service#AutocompletionRequest
-
-  const handleInput = (e) => {
-    setValue(e.target.value);
-  };
-
-  const handleSelect = async (address) => {
-    setValue(address, false);
-    clearSuggestions();
-
-    try {
-      const results = await getGeocode({ address });
-      const { lat, lng } = await getLatLng(results[0]);
-      panTo({ lat, lng });
-    } catch (error) {
-      console.log("😱 Error: ", error);
-    }
-  };
-
-  return (
-    <div className="search">
-      <Combobox onSelect={handleSelect}>
-        <ComboboxInput
-          value={value}
-          onChange={handleInput}
-          disabled={!ready}
-          placeholder="Search your location"
-        />
-        <ComboboxPopover>
-          <ComboboxList>
-            {status === "OK" &&
-              data.map(({ id, description }) => (
-                <ComboboxOption key={id} value={description} />
-              ))}
-          </ComboboxList>
-        </ComboboxPopover>
-      </Combobox>
-    </div>
-  );
 }
