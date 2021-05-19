8aa976eaa7b7dbcde342a0121a561f069ffeaebf
eg-kruglikov <eg.kruglikov@yandex.ru>
2021-05-19 19:54:45 +0300

    cards comleted

--- client/src/components/gig/gig.jsx
+++ client/src/components/gig/gig.jsx
@@ -1,5 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import map from '../../images/map.jpg'
+import './gigModule.scss'
 
 import { getinfoFunc, getplaceFunc } from '../../redux/AC/ac'
 
@@ -29,37 +31,45 @@ export default function Gig(props) {
 
 
   return (
-    <>
-      <div className='homePage' >
-        <div className='homePage-post' style={{ 'color': 'white' }, { 'background-color': 'gray' }} >
-          {gig?.image ? <img alt='post' src={gig?.image} className="homePage-post-img" /> : <img alt='post' src='https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.dreamstime.com%2Fmap-point-location-pin-vector-icon-mapping-satellite-allocation-map-point-location-pin-vector-icon-mapping-image156917586&psig=AOvVaw0jRkoI8fPoEqs41MQrEWSG&ust=1621326164941000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIjM7uKk0PACFQAAAAAdAAAAABAD' className="homePage-post-img" />}
-          <div className='d-flex'>
-            <div className='band-desc'>
-              <div className="d-flex  align-items-center flex-column">
-                <h1 className='homePage-post-title' >{gig?.name}</h1></div>
-              <h4 style={{ 'color': 'white' }}>{gig?.date}</h4>
-              <Link to={`/place/${gig?.adress}`}>
-                <h5 style={{ 'color': 'white' }} onClick={findplaceHandler} data-value={gig?.adress}>{gig?.adress}</h5>
-              </Link>
 
-              <h4 className="homePage-post-text"> тут, возможно, будет описание</h4>
-              {/* <p className='desc-el'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Est necessitatibus deserunt voluptatibus molestias, possimus omnis laborum illum adipisci, similique nostrum commodi totam error laudantium vel at! Itaque ipsam fugit veniam!</p> */}
-              <h5 className='desc-el homePage-post-text'>Лайн-ап:</h5>
-              <ul className='desc-el homePage-post-text'>
-                {gig?.bands ? gig?.bands.map(band => {
-                  return <Link to={`/band/${band}`}>
-                    <li className='desc-el homePage-post-text' onClick={findHandler} data-value={band}>{band}</li>
-                  </Link>
-                })
-                  : null}
-              </ul>
+    <div className='homePage' >
+      <div className="profileBackground" />
+      <div className='gig-card' >
 
-            </div>
-          </div>
+        {gig?.image ? <img alt='post' src={gig?.image} className="homePage-post-img" /> : <img alt='post' src={map} className="gig-img" />}
+
+
+        <div className='cards-conteiner'>
+
+          <div className="d-flex  align-items-center flex-column">
+
+            <h1 className='gig-card-title' >{gig?.name}</h1></div>
+
+
+          <Link className='card-adress' to={`/place/${gig?.adress}`}>
+            <h5 onClick={findplaceHandler} data-value={gig?.adress}>{gig?.adress}</h5>
+          </Link>
+
+          <h4 className='card-adress' > тут, возможно, будет описание</h4>
+
+
+          <h5 className='card-lineup' >Лайн-ап:</h5>
+
+          {gig?.bands ? gig?.bands.map(band => {
+            return <Link className='card-adress' to={`/band/${band}`}>
+              <div className='' onClick={findHandler} data-value={band}>{band}</div>
+            </Link>
+          })
+            : null}
+
+          <h4 className='card-data'>{gig?.date}</h4>
 
         </div>
-      </div>
 
-    </>
+
+      </div>
+    </div>
+
+
   )
 }
