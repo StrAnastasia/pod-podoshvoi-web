03c8acae97525dfb9af19e0c9a81c6da5998583e
tronn333 <tronn333@gmail.com>
2021-06-08 15:30:55 +0300

    before deploy

--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -1,15 +1,11 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import "./headerStyle.scss";
 import hederLogo from "../../images/pngegg.png";
 import profile from "../../images/pofile.png";
 import { Link } from "react-router-dom";
-import Dropdown from "../dropdown/Dropdown";
 import { animateScroll as scroll } from "react-scroll";
 
 export default function Header() {
-  const [auth, setAuth] = useState(null); // IF WE CHANGE THIS INITIAL VALUE WE GET DIFFERENT PAGES
-
   const scrollToBottom = () => {
     scroll.scrollTo(600);
   };
@@ -62,14 +58,6 @@ export default function Header() {
               игруля
             </Link>
           </>
-          <Link
-            style={{ marginRight: 20 }}
-            className="textHeader"
-            aria-current="page"
-            to="/"
-          >
-            новости
-          </Link>
 
           <div style={{ color: "white" }}>|</div>
 
